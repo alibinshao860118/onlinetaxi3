47317e938ea55bc988ea761e2876a49bd0e13cea
alibinshao860118 <liangbinbin@aliyun.com>
2021-08-26 15:17:32 +0800

    online-taxi3

---
+++ 19-cloud-zuul-zuul限流/cloud-zuul/src/main/java/com/mashibing/cloudzuul/CloudZuulApplication.java
@@ -0,0 +1,25 @@
+package com.mashibing.cloudzuul;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class CloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudZuulApplication.class, args);
+    }
+
+
+}
---
+++ 19-cloud-zuul-zuul限流/cloud-zuul/src/main/java/com/mashibing/cloudzuul/service/SentinelService.java
@@ -0,0 +1,24 @@
+package com.mashibing.cloudzuul.service;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.sun.deploy.security.BlockedException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 马士兵教育:晁鹏飞
+ * @date
+ */
+//@Service
+public class SentinelService {
+
+    @SentinelResource(value = "resourceLimit1",blockHandler = "block")
+    public String call(){
+        System.out.println("go");
+        return "go";
+    }
+
+    public String block(BlockedException ex){
+        System.out.println("block");
+        return "block";
+    }
+}
