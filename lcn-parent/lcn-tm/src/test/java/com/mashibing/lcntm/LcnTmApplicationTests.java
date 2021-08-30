afab96cb8c6898a5c0bd73b1d68fabaa4daae731
alibinshao860118 <liangbinbin@aliyun.com>
2021-08-30 17:36:03 +0800

    online-taxi3

---
+++ lcn-parent/lcn-tm/src/test/java/com/mashibing/lcntm/LcnTmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.lcntm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcnTmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
